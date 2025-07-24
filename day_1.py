5c8776dec0c9a582eecf813a906385d11eaa80ca
Alissa <alissa.usen@gmail.com>
2025-07-24 13:54:56 -0700

    practiced how to pritnt

--- day_1.py
+++ day_1.py
@@ -1,1 +1,18 @@
+# I learned how to use print function
+
 print("Hello world")
+
+# learned how to print multiple lines 
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# learned how to print multiple lines as a single line
+print('''1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+2. Knead the dough for 10 minutes.
+3. Add 3g of Salt.
+4. Leave to rise for 2 hours.
+5. Bake at 200 degrees C for 30 minutes.''')
